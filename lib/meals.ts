7792c5d4c6a27673bb8b056f4ac27149453d582c
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-16 17:44:18 +0700

    adding detail meal

--- lib/meals.ts
+++ lib/meals.ts
@@ -4,6 +4,11 @@ const db = sql("meals.db");
 
 export const getAllMeals = async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-//   throw new Error("Something went wrong");
+  //   throw new Error("Something went wrong");
   return db.prepare("SELECT * FROM meals").all();
+};
+
+export const getMeal = async (slug: any) => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get([slug]);
 };
