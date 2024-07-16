3d36d3abb1431193a37f4bd3ed939a453b087642
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-16 11:32:20 +0700

    adding asset and header

---
+++ components/main-header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import classes from "./main-header.module.css";
+import logoImg from "@/assets/logo.png";
+
+export default function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <img src={logoImg.src} alt="NextLevel Logo" />
+      </Link>
+
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
