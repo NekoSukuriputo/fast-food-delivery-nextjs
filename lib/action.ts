dc26cd2d6e3ecb69ef84a15f9f16f3a154aa7d49
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-18 11:00:24 +0700

    adding validation input

--- lib/action.ts
+++ lib/action.ts
@@ -3,6 +3,10 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+const isInvalidText = (text: string) => {
+  return !text || text.trim() === "";
+};
+
 export const shareMeal = async (formData) => {
   const meal = {
     title: formData.get("title"),
@@ -12,6 +16,20 @@ export const shareMeal = async (formData) => {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if(
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.image) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image || meal.image.size === 0
+  ){
+    throw new Error("Invalid input");
+  }
+
   await saveMeal(meal);
   redirect("/meals");
 };
